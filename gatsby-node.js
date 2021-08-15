3627c577dab9f0fdba51c2238f3c3bd6607c1bc9
Daniel <leinpavel@rambler.ru>
2021-08-15 21:47:11 +0300

    Replace then by async await

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,12 +1,9 @@
 const { createFilePath } = require(`gatsby-source-filesystem`);
 const path = require(`path`);
-exports.createPages = ({ actions, graphql }) => {
+
+exports.createPages = async ({ actions, graphql }) => {
   const { createPage } = actions;
-  const blogPostTemplate = path.resolve('./src/components/Post.js');
-
-  //TODO: async await добавить
-
-  return graphql(`
+  const result = await graphql(`
     {
       allMdx(
         sort: { fields: [frontmatter___date], order: DESC }
@@ -22,27 +19,30 @@ exports.createPages = ({ actions, graphql }) => {
         }
       }
     }
-  `).then((result) => {
-    if (result.errors) {
-      throw result.errors;
-    }
-    const posts = result.data.allMdx.nodes;
-    // create page for each mdx node
-    posts.forEach((post, index) => {
-      const previous = index === posts.length - 1 ? null : posts[index + 1];
-      const next = index === 0 ? null : posts[index - 1];
-      createPage({
-        path: post.fields.slug,
-        component: blogPostTemplate,
-        context: {
-          slug: post.fields.slug,
-          previous,
-          next,
-        },
-      });
+  `);
+
+  if (result.errors) {
+    throw result.errors;
+  }
+
+  // create page for each mdx node
+  const blogPostTemplate = path.resolve('./src/components/Post.js');
+  const posts = result.data.allMdx.nodes;
+  posts.forEach((post, index) => {
+    const previous = index === posts.length - 1 ? null : posts[index + 1];
+    const next = index === 0 ? null : posts[index - 1];
+    createPage({
+      path: post.fields.slug,
+      component: blogPostTemplate,
+      context: {
+        slug: post.fields.slug,
+        previous,
+        next,
+      },
     });
   });
 };
+
 exports.onCreateNode = ({ node, actions, getNode }) => {
   const { createNodeField } = actions;
   if (node.internal.type === `Mdx`) {
