fe370bc3472f56c70c80927b674f1a991f7c9eff
Daniel <leinpavel@rambler.ru>
2021-08-16 17:33:45 +0300

    Add the ability to insert images into the post

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,16 +1,26 @@
 module.exports = {
   plugins: [
     {
-      resolve: 'gatsby-plugin-mdx',
-      options: {
-        extensions: ['.mdx', '.md'],
-      },
-    },
-    {
       resolve: 'gatsby-source-filesystem',
       options: {
         path: `${__dirname}/blog`,
         name: 'blog',
+      },
+    },
+    'gatsby-plugin-sharp',
+    'gatsby-remark-images',
+    {
+      resolve: 'gatsby-plugin-mdx',
+      options: {
+        extensions: ['.mdx', '.md'],
+        gatsbyRemarkPlugins: [
+          {
+            resolve: 'gatsby-remark-images',
+            options: {
+              maxWidth: 1200,
+            },
+          },
+        ],
       },
     },
   ],
